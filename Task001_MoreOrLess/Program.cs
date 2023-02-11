6a77f77a1b3f914f817d4393cd1a241b642da3a8
Valentin Chudinov <valentinchudinov@gmail.com>
2023-02-11 15:32:17 +0700

    last commit

--- Task001_MoreOrLess/Program.cs
+++ Task001_MoreOrLess/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿ /* 
+ Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3 
+*/
+
+Console.WriteLine("Enter first number");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter second number");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+if (firstNumber>secondNumber)
+    Console.WriteLine($"number {firstNumber} is greater than {secondNumber} is less");
+else
+    Console.WriteLine($"number {secondNumber} is greater than {firstNumber} is less");
+
---
+++ Task002_MaxNumber/Program.cs
@@ -0,0 +1,17 @@
+﻿/* 
+Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22
+*/
+
+Console.WriteLine("Enter first number");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter second number");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter third number");
+int thirdNumber = Convert.ToInt32(Console.ReadLine());
+
+int result = Math.Max(Math.Max(firstNumber, secondNumber), thirdNumber);
+Console.WriteLine($"The largest number is: {result}");
